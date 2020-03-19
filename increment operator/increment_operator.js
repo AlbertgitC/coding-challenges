e9724250d9cef4fe9bb1c0720bdfcbe763ef81a0
Albert Cheng <lptalbert@gmail.com>
2020-03-19 13:55:22 -0700

    first commit for increment operator

---
+++ increment operator/increment_operator.js
@@ -0,0 +1,10 @@
+// Given an array of integers representing a number, implement the increment operation (++) 
+// such that
+// - arrayIncrement([1, 2]) => [1, 3] // because 12 + 1 => 13
+// - arrayIncrement([1, 9, 9]) => [2, 0, 0] // 199 + 1 => 200
+// - arrayIncrement([9, 9, 9]) => [1, 0, 0, 0] // 999 + 1 => 1000
+
+
+function array_increment(num_arr) {
+   
+}
