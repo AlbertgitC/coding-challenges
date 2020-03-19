f065726ec38b67e3542e33400e232a3f386f3a7b
Albert Cheng <lptalbert@gmail.com>
2020-03-18 19:53:35 -0700

    complete string data structure with hash table

--- substring char frequency/substring_char_frequency.js
+++ substring char frequency/substring_char_frequency.js
@@ -22,8 +22,8 @@ function freq_count(string, char, start_idx, end_idx) {
     return counter;
 }
 
-console.log(freq_count("codingchallenge", "g", 0, 5));
-console.log(freq_count("codingchallenge", "g", 0, 13));
+// console.log(freq_count("codingchallenge", "g", 0, 5));
+// console.log(freq_count("codingchallenge", "g", 0, 13));
 
 // Assume you need the capability to scale out. You will have to search the same list, or string many times for 
 // different character frequencies within a substring. If you were allowed to pre-process the string into a data
@@ -37,3 +37,22 @@ console.log(freq_count("codingchallenge", "g", 0, 13));
 // O(1)
 // assert freq_count(S, c, 0, 5) == 1
 // assert freq_count(S, c, 0, 13) == 2
+
+function process_string(string) {
+    const alph = "abcdefghijklmnopqrstuvwxyz";
+    let hash_table = {};
+    
+    for (let i = 0; i < string.length; i++) {
+        let char = alph.indexOf(string[i]);
+        if (i === 0) {
+            hash_table[i] = new Array(26).fill(0);
+            hash_table[i][char]++;
+        } else {
+            hash_table[i] = hash_table[i - 1];
+            hash_table[i][char]++;
+        };
+    };
+    return hash_table;
+};
+
+console.log(process_string("codingchallenge"));
