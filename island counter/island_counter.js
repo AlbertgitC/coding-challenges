c6648568f9e2fdf290dfc982d3ebb8d39de45608
Albert Cheng <lptalbert@gmail.com>
2020-03-25 15:00:35 -0700

    first commit for island_counter

---
+++ island counter/island_counter.js
@@ -0,0 +1,15 @@
+// Return the number of islands in the given ocean, 0 is ocean, 1  is island.
+// Islands connected horizontally or vertically are counted as one, but not diagonally.
+
+
+// let ocean1 = [
+//   [0, 0, 1, 0, 0],
+//   [1, 1, 1, 0, 0],
+//   [1, 1, 0, 0, 1],
+//   [0, 0, 1, 1, 1],
+//   [1, 1, 0, 1, 1]
+// ];
+
+function islands_counter(ocean) {
+
+};
