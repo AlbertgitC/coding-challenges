401b9dbea03a04179721eb6155acbd872aad3db7
Albert Cheng <lptalbert@gmail.com>
2020-03-24 23:09:57 -0700

    complete permutations

--- permutations/permutations.js
+++ permutations/permutations.js
@@ -10,5 +10,22 @@ function permutation(nums_arr) {
   if (nums_arr.length === 0) return [];
   if (nums_arr.length === 1) return [nums_arr];
 
-  
+  let ans = [];
+  let cur_num = nums_arr.shift();
+  let sub_arrs = permutation(nums_arr);
+
+  sub_arrs.forEach(arr => {
+    for (let i = 0; i <= arr.length; i++) {
+      let cur_combo = arr.slice(0,i).concat([cur_num]).concat(arr.slice(i));
+      ans.push(cur_combo);
+    };
+  });
+
+  return ans;
 }
+
+console.log(permutation([]));
+console.log(permutation([1]));
+console.log(permutation([1, 2]));
+console.log(permutation([1, 2, 3]));
+console.log(permutation([1, 2, 3, 4]));
