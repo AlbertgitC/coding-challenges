3fdcbc45df109db06af506bd6df26c49fbaff159
Albert Cheng <lptalbert@gmail.com>
2020-03-24 22:46:09 -0700

    finish permutaion recursive base cases

---
+++ permutations/permutations.js
@@ -0,0 +1,14 @@
+// Write a function that takes in an array of unique integers and returns an array 
+// of all the permutations of those integers.If the input array is empty, your 
+// function should return an empty array.
+
+// Example:
+// Input: [1, 2, 3]
+// Output: [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 2, 1], [3, 2, 1]]
+
+function permutation(nums_arr) {
+  if (nums_arr.length === 0) return [];
+  if (nums_arr.length === 1) return [nums_arr];
+
+  
+}
