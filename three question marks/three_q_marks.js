e8b0607b8ea51c6a100273a130cfac0796cae40f
Albert Cheng <lptalbert@gmail.com>
2020-03-29 19:39:49 -0700

    first commit for three_q_marks

---
+++ three question marks/three_q_marks.js
@@ -0,0 +1,12 @@
+// Take an input string parameter and determine: For all pairs of digits where 
+// there are exactly 3 question marks between them, do all pairings add up to 10.
+
+// Examples: 
+// strings = [
+//     ("arrb6???4xxbl5???eee5", True),
+//     ("10???0???10", False)
+// ]
+
+function three_q_marks(str) {
+    
+}
