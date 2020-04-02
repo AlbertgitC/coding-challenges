0d24faabd2b8949bfc92f5ba7effd3346e5b22f3
Albert Cheng <lptalbert@gmail.com>
2020-04-01 21:14:52 -0700

    first commit for smallest difference

---
+++ smallest difference/smallest_difference.js
@@ -0,0 +1,15 @@
+// Smallest Difference
+// Write a function that takes in two non-empty arrays of integers, finds the 
+// pair of numbers(one from each array) whos absolute difference is closest to 
+// zero, and returns an array containing these two numbers, with the number 
+// from first array in the first position.
+// Assume that there will only be one pair of numbers with the smallest difference.
+
+// Example:
+// input: array1 = [-1, 5, 10, 20, 28, 3]
+//        array2 = [26, 134, 135, 15, 17]
+// output: [28, 26]
+
+function smallestDifference(array1, array2) {
+    
+}
