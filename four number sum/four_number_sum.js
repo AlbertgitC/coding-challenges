1191a29be4fd7bf3c7e90c24512b86278611f65a
Albert Cheng <lptalbert@gmail.com>
2020-04-06 18:14:35 -0700

    complete brute force four number sum

--- four number sum/four_number_sum.js
+++ four number sum/four_number_sum.js
@@ -19,19 +19,42 @@ function fourNumSum (arr1, targetSum) {
         });
         
         if (sum === targetSum) {
-            return arr1;
+            return [arr1];
         } else { return; };
     };
 
     let currentNum = arr1.shift();
-    arr1.forEach((num, i) => {
+    for (let i = -1; i < arr1.length; i++) {
         let arr2 = arr1.slice(0);
-        arr2.splice(i, 1, currentNum);
+        if (i !== -1) { arr2.splice(i, 1, currentNum); };
         let subRes = fourNumSum(arr2, targetSum);
-        if (subRes) { result.push(subRes); };
-    });
+        if (subRes) { result = result.concat(subRes); };
+    };
+    result = checkUnique(result);
 
     return result;
 }
 
-console.log(fourNumSum([7, 6, 4, -1, 1], 16));
+function checkUnique(twoDArr) {
+    let result = [];
+    for (let i = 0; i < twoDArr.length; i++) {
+        let unique = true;
+        for (let j = i + 1; j < twoDArr.length; j++) {
+            let subArr = [];
+            
+            twoDArr[i].forEach(ele => {
+                if (twoDArr[j].includes(ele)) subArr.push(ele);
+            });
+            if (subArr.length === twoDArr[i].length) {
+                unique = false;
+            };
+            
+        };
+        if (unique) { result.push(twoDArr[i]) };
+    };
+    
+    return result;
+}
+
+console.log(fourNumSum([-10, -3, -5, 2, 15, -7, 28, -6, 12, 8, 11, 5], 20));
+console.log(fourNumSum([1, 2, 3, 4, 5], 100));
