7c062d78ed743284e095b69877f4bcc2e8a4718a
Albert Cheng <lptalbert@gmail.com>
2020-04-08 19:16:07 -0700

    complete max path sum

--- max path sum/max_path_sum.js
+++ max path sum/max_path_sum.js
@@ -1,28 +1,28 @@
 // Write a function that takes in a Binary Tree and returns its max path sum
 
 function maxPathSum(tree) {
-    let max = -Infinity;
-    let curMPS = recurMPS(tree, max);
-    return Math.max(max, curMPS);
+    let curMPS = recurMPS(tree);
+    return Math.max(curMPS[0], curMPS[1]);
 }
 
-function recurMPS(node, max) {
-    if (node.left === null && node.right === null) return node.value;
-    let leftSum = -Infinity;
-    let rightSum = -Infinity;
-
+function recurMPS(node, max = -Infinity) {
+    if (node.left === null && node.right === null) return [node.value, node.value];
+    let leftSum = [-Infinity, -Infinity];
+    let rightSum = [-Infinity, -Infinity];
+    let curMax = max;
     if (node.left) {
         leftSum = recurMPS(node.left, max);
+        if (curMax < leftSum[1]) curMax = leftSum[1];
     };
     if (node.right) {
         rightSum = recurMPS(node.right, max);
+        if (curMax < rightSum[1]) curMax = rightSum[1];
     };
 
-    let curLeftSum = node.value + leftSum;
-    let curRightSum = node.value + rightSum;
-    if (node.left !== null && node.right !== null) {
-        if (max < curLeftSum + rightSum) max = curLeftSum + rightSum;
-    };
+    let curLeftSum = node.value + leftSum[0];
+    let curRightSum = node.value + rightSum[0];
+    
+    curMax = Math.max(node.value, curLeftSum, curRightSum, curMax, (curLeftSum + rightSum[0]));
 
-    return Math.max(node.value, curLeftSum, curRightSum);
+    return [Math.max(node.value, curLeftSum, curRightSum), curMax];
 }
