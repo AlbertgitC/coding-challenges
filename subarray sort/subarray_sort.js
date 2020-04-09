d4c0254eec9a3af82333e3296ab0771cca1b4372
Albert Cheng <lptalbert@gmail.com>
2020-04-08 21:17:49 -0700

    complete brute force subarray sort

--- subarray sort/subarray_sort.js
+++ subarray sort/subarray_sort.js
@@ -1,5 +1,29 @@
 // Subarray Sort
 
 function subarraySort(array) {
+    let result = null;
+    for (let i = 0; i < array.length - 1; i++) {
+        const curNum = array[i];
+        const nextNum = array[i + 1];
+        if (curNum > nextNum) {
+            result = backSort(array, i + 1, result);
+        }
+    };
 
+    if (result) return result;
+    return [-1, -1];
+}
+
+function backSort(array, idx, result) {
+    if (array[idx] >= array[idx - 1] || idx === 0) return result;
+    let curNum = array[idx - 1]
+    array[idx - 1] = array[idx];
+    array[idx] = curNum;
+    if (!result) {
+        result = [idx - 1, idx];
+    } else {
+        if (result[0] > (idx - 1)) result[0] = idx - 1;
+        if (result[1] < idx) result[1] = idx;
+    };
+    return backSort(array, idx - 1, result);
 }
