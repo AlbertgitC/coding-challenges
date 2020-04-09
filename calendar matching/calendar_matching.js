d1cff36b8b1661efc1c3fc459fc33bd4499d795c
Albert Cheng <lptalbert@gmail.com>
2020-04-09 16:28:41 -0700

    attempt merge calendar1 for calendar matching

---
+++ calendar matching/calendar_matching.js
@@ -0,0 +1,49 @@
+// Calendar Matching
+
+function calendarMatching(calendar1, dailyBounds1, calendar2, dailyBounds2, meetingDuration) {
+    let commonDB = [];
+    let commonCal = [];
+
+    if (toNum(dailyBounds1[0]) < toNum(dailyBounds2[0])) {
+        commonDB.push(toNum(dailyBounds2[0]));
+    } else {
+        commonDB.push(toNum(dailyBounds1[0]));
+    };
+
+    if (toNum(dailyBounds1[1]) < toNum(dailyBounds2[1])) {
+        commonDB.push(toNum(dailyBounds1[1]));
+    } else {
+        commonDB.push(toNum(dailyBounds2[1]));
+    };
+
+    if (commonDB[0] >= commonDB[1]) return [];
+
+    if (calendar1.length) {
+        for (let i = 0; i < calendar1.length; i++) {
+            let start = toNum(calendar1[i][0]);
+            let end = toNum(calendar1[i][1]);
+            if (i === 0 && start >= commonDB[1]) {
+                break;
+            } else if (i === calendar1.length - 1 && end <= commonDB[0]) {
+                break;
+            };
+            if (commonCal.length === 0 && end > commonDB[0] && start < commonDB[1]) {
+                commonCal.push([start, end]);
+            } else if (commonCal.length !== 0 && end > commonDB[0] && start < commonDB[1]) {
+                let lastBlock = commonCal[commonCal.length - 1];
+                if (start = lastBlock[1]) {
+                    commonCal.pop();
+                    commonCal.push([lastBlock[0], end]);
+                } else {
+                    commonCal.push([start, end]);
+                };
+            };
+        };
+    }
+
+}
+
+function toNum(str) {
+    let regexDigit = /\d/g
+    return parseInt(str.match(regexDigit).join(""));
+}
