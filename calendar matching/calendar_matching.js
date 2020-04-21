1b86b3fb5ad61460d1f7e3db0d5a28d9468f347f
Albert Cheng <lptalbert@gmail.com>
2020-04-20 22:15:44 -0700

    different approach to match calendar

--- calendar matching/calendar_matching.js
+++ calendar matching/calendar_matching.js
@@ -18,47 +18,50 @@ function calendarMatching(calendar1, dailyBounds1, calendar2, dailyBounds2, meet
 
     if (commonDB[0] >= commonDB[1]) return [];
 
-    if (calendar1.length) {
-        for (let i = 0; i < calendar1.length; i++) {
-            let start = toNum(calendar1[i][0]);
-            let end = toNum(calendar1[i][1]);
-            if (i === 0 && start >= commonDB[1]) {
-                break;
-            } else if (i === calendar1.length - 1 && end <= commonDB[0]) {
-                break;
-            };
-            if (commonCal.length === 0 && end > commonDB[0] && start < commonDB[1]) {
-                commonCal.push([start, end]);
-            } else if (commonCal.length !== 0 && end > commonDB[0] && start < commonDB[1]) {
-                let lastBlock = commonCal[commonCal.length - 1];
-                if (start - lastBlock[1] < meetingDuration) {
-                    commonCal.pop();
-                    commonCal.push([lastBlock[0], end]);
-                } else {
-                    commonCal.push([start, end]);
-                };
-            };
-        };
-    };
+    // if (calendar1.length) {
+    //     for (let i = 0; i < calendar1.length; i++) {
+    //         let start = toNum(calendar1[i][0]);
+    //         let end = toNum(calendar1[i][1]);
+    //         if (i === 0 && start >= commonDB[1]) {
+    //             break;
+    //         } else if (i === calendar1.length - 1 && end <= commonDB[0]) {
+    //             break;
+    //         };
+    //         if (commonCal.length === 0 && end > commonDB[0] && start < commonDB[1]) {
+    //             commonCal.push([start, end]);
+    //         } else if (commonCal.length !== 0 && end > commonDB[0] && start < commonDB[1]) {
+    //             let lastBlock = commonCal[commonCal.length - 1];
+    //             if (start - lastBlock[1] < meetingDuration) {
+    //                 commonCal.pop();
+    //                 commonCal.push([lastBlock[0], end]);
+    //             } else {
+    //                 commonCal.push([start, end]);
+    //             };
+    //         };
+    //     };
+    // };
 
-    if (calendar2.length) {
-        let checkIdx = 0;
+    // if (calendar2.length) {
+    //     let checkIdx = 0;
 
-        for (let i = 0; i < calendar2.length; i++) {
-            let start = toNum(calendar2[i][0]);
-            let end = toNum(calendar2[i][1]);
-            if (start >= commonDB[1] || end <= commonDB[0]) {
-                continue;
-            };
+    //     for (let i = 0; i < calendar2.length; i++) {
+    //         let start = toNum(calendar2[i][0]);
+    //         let end = toNum(calendar2[i][1]);
+    //         if (start >= commonDB[1] || end <= commonDB[0]) {
+    //             continue;
+    //         };
 
-            if (commonCal.length === 0 && end > commonDB[0] && start < commonDB[1]) {
-                commonCal.push([start, end]);
-            } else if (commonCal.length !== 0 && end > commonDB[0] && start < commonDB[1]) {
+    //         if (commonCal.length === 0) {
+    //             commonCal.push([start, end]);
+    //         } else {
                 
-            };
-        };
+    //         };
+    //     };
         
-    };
+    // };
+
+    //different approach, keep track of available blocks only
+    
 
     console.log(commonCal);
 
