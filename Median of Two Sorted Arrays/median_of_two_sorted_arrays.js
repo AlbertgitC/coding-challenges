2c7070a34646b921a7f0605282f862e0f5de86e5
Albert Cheng <lptalbert@gmail.com>
2020-12-11 21:14:33 -0800

    first attempt

--- Median of Two Sorted Arrays/median_of_two_sorted_arrays.js
+++ Median of Two Sorted Arrays/median_of_two_sorted_arrays.js
@@ -1,3 +1,40 @@
 /* Given two sorted arrays nums1 and nums2 of size m and n respectively, 
 return the median of the two sorted arrays. */
 
+var findMedianSortedArrays = function (nums1, nums2) {
+    const merged = [];
+    let curIdx1 = 0;
+    let curIdx2 = 0;
+    while (curIdx1 < nums1.length || curIdx2 < nums2.length) {
+        let num1 = nums1[curIdx1];
+        let num2 = nums2[curIdx2];
+        if (num1 === undefined) {
+            merged.concat(nums2.slice(curIdx2));
+            break;
+        } else if (num2 === undefined) {
+            merged.concat(nums1.slice(curIdx1));
+            break;
+        };
+
+        if (num1 > num2) {
+            merged.push(num2);
+            curIdx2++;
+        } else {
+            merged.push(num1);
+            curIdx1++;
+        };
+    };
+    console.log(merged);
+    let ans;
+    if (merged.length % 2 === 0) {
+        let mid = merged.length / 2;
+        ans = (merged[mid] + merged[mid - 1]) / 2;
+    } else {
+        let mid = Math.floor(merged.length / 2);
+        ans = merged[mid];
+    }
+
+    return ans;
+};
+
+console.log(findMedianSortedArrays([1, 3], [2]));
