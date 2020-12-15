9d1ffc73631eb125ed72636375d9c9192b230c72
Albert Cheng <lptalbert@gmail.com>
2020-12-14 21:56:33 -0800

    first attempt, brute force solved

--- Longest Palindromic Substring/Longest_Palindromic_Substring.js
+++ Longest Palindromic Substring/Longest_Palindromic_Substring.js
@@ -1,3 +1,20 @@
-function longestPalindrome(string) {
-    
+function longestPalindrome(str) {
+    let longest = str[0];
+    let startIdx = 0;
+    let endIdx = 1;
+    const charsArr = str.split("");
+    while (startIdx < str.length) {
+        let subStr = charsArr.slice(startIdx, endIdx);
+        let str1 = subStr.join("");
+        let str2 = subStr.reverse().join("");
+        if (str1 === str2 && str1.length > longest.length) {
+            longest = str1;
+        };
+        endIdx++;
+        if (endIdx > str.length) {
+            startIdx++;
+            endIdx = startIdx + 1;
+        }
+    };
+    return longest;
 };
