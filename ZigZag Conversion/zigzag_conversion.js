ec35b9f93b7a7d98a2979f7a30b8dbac5c7bb978
Albert Cheng <lptalbert@gmail.com>
2020-12-15 22:29:32 -0800

    first attempt

---
+++ ZigZag Conversion/zigzag_conversion.js
@@ -0,0 +1,32 @@
+// leet code #6
+
+function convert(s, numRows) {
+    if (numRows === 1) return s;
+    const matrix = new Array(numRows).fill(new Array());
+    let down = true;
+    let curRow = 0;
+    for (let i = 0; i < s.length; i++) {
+        if (down) {
+            matrix[curRow].push(s[i]);
+            if (curRow === numRows - 1) {
+                down = false;
+                curRow--;
+            } else {
+                curRow++;
+            };
+        } else {
+            matrix[curRow].push(s[i]);
+            if (curRow === 0) {
+                down = true;
+                curRow++;
+            } else {
+                curRow--;
+            };
+        }; 
+    };
+    let ans = "";
+    for (let row of matrix) {
+        ans += row.join("");
+    }
+    return ans;
+};
