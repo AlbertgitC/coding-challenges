805c83a1371596db7b25302cbfaeff763560a270
Albert Cheng <lptalbert@gmail.com>
2020-12-18 22:03:31 -0800

    second attempt

--- Reverse Integer/Reverse_Integer.js
+++ Reverse Integer/Reverse_Integer.js
@@ -1,19 +1,28 @@
 function reverse(x) {
     if (x < 10 && x > -10) return x;
     let number = Math.abs(x);
-    const reversed_digits = [];
-    while (number > 0) {
-        let current_digit = number % 10;
-        reversed_digits.unshift(current_digit);
-        number = Math.floor(number / 10);
-    };
+    // const reversed_digits = [];
+    // while (number > 0) {
+    //     let current_digit = number % 10;
+    //     reversed_digits.unshift(current_digit);
+    //     number = Math.floor(number / 10);
+    // };
+    // let ans = 0;
+    // for (let i = 0; i < reversed_digits.length; i++) {
+    //     ans = ans + reversed_digits[i] * (10 ** i)
+    // };
+
+    // if (x < 0) return ans * -1;
+    // return ans;
+
     let ans = 0;
-    for (let i = 0; i < reversed_digits.length; i++) {
-        ans = ans + reversed_digits[i] * (10 ** i)
+    while (number > 0) {
+        ans = ans * 10 + number % 10;
+        number = Math.trunc(number / 10);
     };
 
     if (x < 0) return ans * -1;
     return ans;
 };
 
-console.log(reverse(-102))
+console.log(reverse(100))
