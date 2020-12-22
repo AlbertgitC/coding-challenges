74e48090f9cd29a4df54763718f10ac813c06a25
Albert Cheng <lptalbert@gmail.com>
2020-12-21 21:45:52 -0800

    first attempt

--- String to Integer_atoi/String_to_Integer.js
+++ String to Integer_atoi/String_to_Integer.js
@@ -1,5 +1,28 @@
 // leetCode #8
 
+const nums = new Set(["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]);
+
 function myAtoi(string) {
-    
+    let str = string.split(" ").join("");
+    if (str[0] === "-" && nums.has(str[1])) {
+        let numStr = "-" + getNumString(str.slice(1));
+        return Number(numStr);
+    } else if (nums.has(str[0])) {
+        let numStr = getNumString(str);
+        return Number(numStr);
+    } else {
+        return 0;
+    };
 };
+
+function getNumString(str) {
+    let numStr = "";
+    for (let i = 0; i < str.length; i++) {
+        if (nums.has(str[i])) {
+            numStr += str[i];
+        } else {
+            break;
+        };
+    };
+    return numStr;
+}
