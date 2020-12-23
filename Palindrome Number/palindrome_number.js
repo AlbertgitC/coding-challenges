4517f5f78ac2e007da69100cbf563ccae60993b0
Albert Cheng <lptalbert@gmail.com>
2020-12-22 23:20:44 -0800

    first attempt

--- Palindrome Number/palindrome_number.js
+++ Palindrome Number/palindrome_number.js
@@ -1,5 +1,13 @@
 // leetCode #9
 
 function isPalindrome(x) {
-
+    if (x < 0) return false;
+    let num = x;
+    let palindromeNum = 0;
+    while (num > 0) {
+        palindromeNum = palindromeNum * 10 + num % 10;
+        num = Math.trunc(num / 10);
+    };
+    if (palindromeNum === x) return true;
+    return false;
 };
