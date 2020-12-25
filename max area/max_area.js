17bc6cdcf6c33da87ef7be3232c6b34b01a6133d
Albert Cheng <lptalbert@gmail.com>
2020-12-24 16:00:54 -0800

    solve with brute force attempt

--- max area/max_area.js
+++ max area/max_area.js
@@ -1,5 +1,13 @@
 // leetCode #11
 
 function maxArea(height) {
-    
+    let maxA = 0;
+    for (let i = 0; i < height.length - 1; i++) {
+        for (let j = i + 1; j < height.length; j++) {
+            let lower = height[i] < height[j] ? height[i] : height[j];
+            let area = lower * (j - i);
+            maxA = Math.max(maxA, area);
+        };
+    };
+    return maxA;
 };
