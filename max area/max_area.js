634da396bb19f3e7b7c75fe49aceb31afc16cd09
Albert Cheng <lptalbert@gmail.com>
2020-12-24 16:12:27 -0800

    solve with O(n)

--- max area/max_area.js
+++ max area/max_area.js
@@ -1,12 +1,30 @@
 // leetCode #11
 
 function maxArea(height) {
-    let maxA = 0;
-    for (let i = 0; i < height.length - 1; i++) {
-        for (let j = i + 1; j < height.length; j++) {
-            let lower = height[i] < height[j] ? height[i] : height[j];
-            let area = lower * (j - i);
+    // brute force O(n ** 2)
+    // let maxA = 0;
+    // for (let i = 0; i < height.length - 1; i++) {
+    //     for (let j = i + 1; j < height.length; j++) {
+    //         let lower = height[i] < height[j] ? height[i] : height[j];
+    //         let area = lower * (j - i);
+    //         maxA = Math.max(maxA, area);
+    //     };
+    // };
+    // return maxA;
+
+    // O(n)
+    let startIdx = 0;
+    let endIdx = height.length - 1;
+    let maxA = 0
+    while (startIdx < endIdx) {
+        if (height[startIdx] < height[endIdx]) {
+            let area = height[startIdx] * (endIdx - startIdx);
             maxA = Math.max(maxA, area);
+            startIdx++;
+        } else {
+            let area = height[endIdx] * (endIdx - startIdx);
+            maxA = Math.max(maxA, area);
+            endIdx--;
         };
     };
     return maxA;
