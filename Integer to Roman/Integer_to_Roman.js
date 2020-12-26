0392cd71633fa9380fb965e73131e9a9baea84c3
Albert Cheng <lptalbert@gmail.com>
2020-12-25 18:28:39 -0800

    attemp single digit

--- Integer to Roman/Integer_to_Roman.js
+++ Integer to Roman/Integer_to_Roman.js
@@ -1,5 +1,38 @@
 // leetCode #12
 
-function intToRoman(num) {
+// I             1
+// V             5
+// X             10
+// L             50
+// C             100
+// D             500
+// M             1000
 
+// 1 <= num <= 3999
+
+function intToRoman(num) {
+    let roman = "";
+    let digits = Math.trunc(Math.log10(num)) + 1;
+    while (num > 0) {
+        let curDigit = num % 10;
+        if (curDigit < 4) {
+            roman = lessThree(roman, "I", curDigit);
+        } else if (curDigit === 4) {
+            roman = "IV";
+        } else if (curDigit === 5) {
+            roman = "V";
+        } else if (curDigit < 9) {
+            roman = "V" + lessThree(roman, "I", curDigit);
+        } else if (curDigit === 9) {
+            roman = "IX";
+        };
+        num = Math.trunc(num / 10);
+    };
+};
+
+function lessThree(string, sym, num) {
+    for (let i = 0; i < num; i++) {
+        string += sym;
+    };
+    return string;
 };
