e7313cf52937966f66374e0ce92a556f1edfc476
Albert Cheng <lptalbert@gmail.com>
2020-12-25 18:47:56 -0800

    commit for first solution

--- Integer to Roman/Integer_to_Roman.js
+++ Integer to Roman/Integer_to_Roman.js
@@ -11,28 +11,35 @@
 // 1 <= num <= 3999
 
 function intToRoman(num) {
+    const sets = [["I", "V"], ["X", "L"], ["C", "D"], ["M"]];
     let roman = "";
-    let digits = Math.trunc(Math.log10(num)) + 1;
+    let curDigitPos = 0;
     while (num > 0) {
+        let curSet = sets[curDigitPos];
         let curDigit = num % 10;
         if (curDigit < 4) {
-            roman = lessThree(roman, "I", curDigit);
+            roman = lessThree(curSet[0], curDigit) + roman;
         } else if (curDigit === 4) {
-            roman = "IV";
+            roman = curSet[0] + curSet[1] + roman;
         } else if (curDigit === 5) {
-            roman = "V";
+            roman = curSet[1] + roman;
         } else if (curDigit < 9) {
-            roman = "V" + lessThree(roman, "I", curDigit);
+            roman = curSet[1] + lessThree(curSet[0], curDigit - 5) + roman;
         } else if (curDigit === 9) {
-            roman = "IX";
+            roman = curSet[0] + sets[curDigitPos + 1][0] + roman;
         };
         num = Math.trunc(num / 10);
+        curDigitPos++;
     };
+    return roman;
 };
 
-function lessThree(string, sym, num) {
+function lessThree(sym, num) {
+    let string = ""
     for (let i = 0; i < num; i++) {
         string += sym;
     };
     return string;
 };
+
+console.log(intToRoman(1342))
