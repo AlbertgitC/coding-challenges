f7779b49cf013b8677583c691d3d345c929b7f3a
Albert Cheng <lptalbert@gmail.com>
2021-01-05 23:52:48 -0800

    first attempt with for loop

--- Array Concatenation/array_concatenation.js
+++ Array Concatenation/array_concatenation.js
@@ -1,3 +1,11 @@
 function canFormArray(arr, pieces) {
-    
+    for (let i = 0; i < arr.length; i++) {
+        let curNum = arr[i];
+        for (let j = 0; j < pieces.length; j++) {
+            let curPiece = pieces[j];
+            if (curPiece[0] === curNum) {
+                
+            };
+        };
+    };
 };
