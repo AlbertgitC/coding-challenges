316a6c7bcd03c1747316c19aff6997f540539659
Albert Cheng <lptalbert@gmail.com>
2021-01-06 00:33:49 -0800

    solve with while loop

--- Array Concatenation/array_concatenation.js
+++ Array Concatenation/array_concatenation.js
@@ -1,11 +1,21 @@
 function canFormArray(arr, pieces) {
-    for (let i = 0; i < arr.length; i++) {
-        let curNum = arr[i];
-        for (let j = 0; j < pieces.length; j++) {
-            let curPiece = pieces[j];
-            if (curPiece[0] === curNum) {
-                
+    let curIdx = 0;
+    while (curIdx < arr.length) {
+        let beginIdx = curIdx;
+        for (let i = 0; i < pieces.length; i++) {
+            let curPiece = pieces[i];
+            if (arr[curIdx] === curPiece[0]) {
+                curIdx++;
+                for (let j = 1; j < curPiece.length; j++) {
+                    if (arr[curIdx] !== curPiece[j]) {
+                        return false;
+                    } else {
+                        curIdx++;
+                    };
+                };
             };
         };
+        if (beginIdx === curIdx) return false;
     };
+    return true;
 };
