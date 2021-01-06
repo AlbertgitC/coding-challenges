684720363a1b4ba07421f53c16d91c4f2e2a4d4a
Albert Cheng <lptalbert@gmail.com>
2021-01-06 15:40:09 -0800

    first attempt

--- Kth Missing Positive Number/kth_missing_num.js
+++ Kth Missing Positive Number/kth_missing_num.js
@@ -1,3 +1,14 @@
 function findKthPositive(arr, k) {
-
+    let curNum = 1;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === curNum) {
+            curNum++;
+        } else {
+            k--;
+            if (k === 0) return curNum;
+            curNum++;
+            i--;
+        };
+    };
+    return curNum + k - 1;
 };
