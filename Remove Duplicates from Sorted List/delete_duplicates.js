514b6a5dceac3634422e487dab5d836b359b093f
Albert Cheng <lptalbert@gmail.com>
2021-01-07 19:50:46 -0800

    first attempt

--- Remove Duplicates from Sorted List/delete_duplicates.js
+++ Remove Duplicates from Sorted List/delete_duplicates.js
@@ -11,5 +11,28 @@
  */
 
 function deleteDuplicates(head) {
-
+    let curVal = head.val;
+    let curNote = head;
+    let checkedNote = null;
+    let newHead = null;
+    let duplicated = false;
+    while (curNote) {
+        if (curVal !== curNote.next.val && !duplicated) {
+            if (!newHead) newHead = curNote;
+            checkedNote = curNote;
+            curVal = curNote.next.val;
+            curNote = curNote.next;
+        } else if (curVal !== curNote.next.val && duplicated) {
+            duplicated = false;
+            if (checkedNote) {
+                checkedNote.next = curNote.next;
+            };
+            curVal = curNote.next.val;
+            curNote = curNote.next;
+        } else {
+            duplicated = true;
+            curNote = curNote.next;
+        };
+    };
+    return newHead;
 };
