19b05847bf70c4ef979f0415b67e9f2f2eab3c85
Albert Cheng <lptalbert@gmail.com>
2021-01-08 22:20:02 -0800

    first attempt

--- Two String Arrays Checker/two _string_arrays_checker.js
+++ Two String Arrays Checker/two _string_arrays_checker.js
@@ -1,3 +1,11 @@
 function arrayStringsAreEqual(word1, word2) {
+    let string1 = spell(word1);
+    let string2 = spell(word2);
+    if (string1 === string2) return true;
+    return false;
+};
 
+function spell(arr) {
+    const reducer = (accumulator, curString) => accumulator + curString;
+    return arr.reduce(reducer);
 };
