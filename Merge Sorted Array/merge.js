681e47c8237b3e56343c031777383ea1797d2b2d
Albert Cheng <lptalbert@gmail.com>
2021-01-11 11:55:17 -0800

    first attempt with for loop

--- Merge Sorted Array/merge.js
+++ Merge Sorted Array/merge.js
@@ -1,3 +1,11 @@
 function merge(nums1, m, nums2, n) {
-
+    let idx = 0;
+    for (let i = 0; i < nums2.length; i++) {
+        let curNum1 = nums1[idx];
+        let curNum2 = nums2[i];
+        if (curNum2 < curNum1) {
+            nums1.splice(idx, 0, curNum2);
+            idx++;
+        }
+    };
 };
