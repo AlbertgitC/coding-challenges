a6f7f1591587308d3737a621297ea5b595512472
Albert Cheng <lptalbert@gmail.com>
2021-01-10 17:13:48 -0800

    fix while loop bug

--- Sorted Array With Instructions/sorted_array_instructions.js
+++ Sorted Array With Instructions/sorted_array_instructions.js
@@ -1,23 +1,21 @@
 function createSortedArray(instructions) {
-    let nums = [];
+    let nums = [instructions[0]];
     let cost = 0;
     
     
-    for (let i = 0; i < instructions.length; i++) {
-        if (nums.length === 0) {
-            nums.push(instructions[i]);
-            continue;
-        };
+    for (let i = 1; i < instructions.length; i++) {
         let curNum = instructions[i];
         let idx = 0;
         let equal = 0;
         let target = nums[idx];
         let sorted = false;
-        while (!sorted && target) {
+        while (!sorted && target !== undefined) {
             if (curNum > target) {
                 idx++;
+                target = nums[idx];
             } else if (curNum === target) {
                 idx++;
+                target = nums[idx];
                 equal++;
             } else {
                 sorted = true;
@@ -30,3 +28,5 @@ function createSortedArray(instructions) {
 
     return cost;
 };
+
+console.log(createSortedArray([1, 5, 6, 2]));
