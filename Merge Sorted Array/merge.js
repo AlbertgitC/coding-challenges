3113af1a037ec47360da895eae0d6526dbb920fb
Albert Cheng <lptalbert@gmail.com>
2021-01-11 12:12:47 -0800

    attempt with while loop

--- Merge Sorted Array/merge.js
+++ Merge Sorted Array/merge.js
@@ -1,11 +1,20 @@
 function merge(nums1, m, nums2, n) {
-    let idx = 0;
-    for (let i = 0; i < nums2.length; i++) {
-        let curNum1 = nums1[idx];
-        let curNum2 = nums2[i];
-        if (curNum2 < curNum1) {
-            nums1.splice(idx, 0, curNum2);
-            idx++;
+    let idx1 = 0;
+    let idx2 = 0;
+    while (idx2 < nums2.length) {
+        let num1 = nums1[idx1];
+        let num2 = nums2[idx2];
+        if (num1 === undefined) {
+            for (let i = idx2; i < nums2.length; i++) {
+                nums1.push(nums2[i]);
+            };
+            break;
+        } else if (num2 < num1) {
+            nums1.splice(idx1, 0, num2);
+            idx1++;
+            idx2++;
+        } else {
+            idx1++;
         }
     };
 };
