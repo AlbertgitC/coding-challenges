81fa4e2ea8fac5d92eb2a98019e5d1e04f567ea0
Albert Cheng <lptalbert@gmail.com>
2021-01-13 23:46:38 -0800

    attempt with while loop

--- Boats to Save People/boats_to_save_people.js
+++ Boats to Save People/boats_to_save_people.js
@@ -1,3 +1,24 @@
 function numRescueBoats(people, limit) {
+    people.sort(function (a, b) {
+        return a - b;
+    });
+    let boats = 0;
+    while (people.length > 0) {
+        if (people.length === 1) {
+            boats++;
+            break;
+        };
+        let lightest = people[0];
+        let heaviest = people[people.length - 1];
 
+        if (lightest + heaviest <= limit) {
+            people.pop();
+            people.shift();
+            boats++;
+        } else {
+            people.pop();
+            boats++;
+        };
+    };
+    return boats;
 };
