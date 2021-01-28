b99bb57197c7f030214b49ac33fbbdf7eed140ff
Albert Cheng <lptalbert@gmail.com>
2021-01-27 22:30:50 -0800

    fix bigInt problem

--- Concatenation of Binary Numbers/concat_binary_nums.js
+++ Concatenation of Binary Numbers/concat_binary_nums.js
@@ -1,10 +1,20 @@
 function concatenatedBinary(n) {
-    let binaryString = "";
+    // let binaryString = "";
+
+    // for (let i = 1; i <= n; i++) {
+    //     let numString = i.toString(2);
+    //     binaryString += numString;
+    // };
+    
+    // return parseInt(binaryString, 2) % (10 ** 9 + 7);
+
+    let num = 0;
 
     for (let i = 1; i <= n; i++) {
-        let numString = i.toString(2);
-        binaryString += numString;
+        num *= (2 ** i.toString(2).length);
+        num += i;
+        num %= (10 ** 9 + 7);
     };
 
-    return parseInt(binaryString, 2);
+    return num;
 };
