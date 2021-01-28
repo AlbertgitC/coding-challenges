fef2678fa9fb267b3fd30f360026ddac00ecb29d
Albert Cheng <lptalbert@gmail.com>
2021-01-27 21:39:30 -0800

    first attempt

--- Concatenation of Binary Numbers/concat_binary_nums.js
+++ Concatenation of Binary Numbers/concat_binary_nums.js
@@ -1,3 +1,10 @@
 function concatenatedBinary(n) {
+    let binaryString = "";
 
+    for (let i = 1; i <= n; i++) {
+        let numString = i.toString(2);
+        binaryString += numString;
+    };
+
+    return parseInt(binaryString, 2);
 };
