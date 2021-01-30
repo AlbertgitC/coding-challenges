326996c17f14b839e6bc80c7d49b10a4d27b7525
Albert Cheng <lptalbert@gmail.com>
2021-01-30 00:59:00 -0800

    solve with hash table

--- Vertical Order Binary Tree/vertical_traversal.js
+++ Vertical Order Binary Tree/vertical_traversal.js
@@ -1,27 +1,37 @@
 function verticalTraversal(root) {
     const table = {};
-    traverse(root, 0, table);
+    traverse(root, [0, 0], table);
     let cols = Object.keys(table);
     cols.sort((a, b) => a - b);
     let result = [];
     for (col of cols) {
-        result.push(table[col]);
+        let rows = Object.keys(table[col]);
+        rows.sort((a, b) => a - b);
+        let colVals = [];
+        for (row of rows) {
+            colVals = colVals.concat(table[col][row]);
+        }
+        result.push(colVals);
     };
     return result;
 };
 
-function traverse(node, col, table) {
+function traverse(node, [row, col], table) {
     if (node === null) return;
     
     
-    traverse(node.left, col - 1, table);
+    traverse(node.left, [row + 1, col - 1], table);
     
     if (col in table) {
-        table[col].push(node.val);
-        table[col] = table[col].sort((a, b) => a - b);
+        if (row in table[col]) {
+            table[col][row].push(node.val);
+            table[col][row].sort((a, b) => a - b);
+        } else {
+            table[col][row] = [node.val];
+        };
     } else {
-        table[col] = [node.val];
+        table[col] = {[row]: [node.val]};
     };
 
-    traverse(node.right, col + 1, table);
+    traverse(node.right, [row + 1, col + 1], table);
 };
