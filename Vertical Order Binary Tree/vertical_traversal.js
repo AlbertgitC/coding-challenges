e084a3cfb9d62034820d32ec4af46f5b49e36e20
Albert Cheng <lptalbert@gmail.com>
2021-01-30 00:02:11 -0800

    first attempt

--- Vertical Order Binary Tree/vertical_traversal.js
+++ Vertical Order Binary Tree/vertical_traversal.js
@@ -1,7 +1,27 @@
 function verticalTraversal(root) {
-
+    const table = {};
+    traverse(root, 0, table);
+    let cols = Object.keys(table);
+    cols.sort((a, b) => a - b);
+    let result = [];
+    for (col of cols) {
+        result.push(table[col]);
+    };
+    return result;
 };
 
 function traverse(node, col, table) {
+    if (node === null) return;
     
+    
+    traverse(node.left, col - 1, table);
+    
+    if (col in table) {
+        table[col].push(node.val);
+        table[col] = table[col].sort((a, b) => a - b);
+    } else {
+        table[col] = [node.val];
+    };
+
+    traverse(node.right, col + 1, table);
 };
